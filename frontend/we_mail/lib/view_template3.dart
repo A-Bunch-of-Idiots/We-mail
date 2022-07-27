c07848150e568610fac0b4c30d5e4a2e04e10c9a
Prathushan Inparaj <prathushan99@gmail.com>
2022-07-27 18:40:34 +0530

    view_template changes

--- frontend/we_mail/lib/view_template3.dart
+++ frontend/we_mail/lib/view_template3.dart
@@ -1,56 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:auto_size_text/auto_size_text.dart';
 
-void main() {
-  runApp(const viewTemplate());
-}
-
-class viewTemplate extends StatelessWidget {
-  const viewTemplate({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'We-Mail',
-      theme: ThemeData(
-        // Define the default brightness and colors.
-        brightness: Brightness.dark,
-        primaryColor: Colors.lightBlue[800],
-
-        // Define the default font family.
-        fontFamily: 'Georgia',
-
-        // Define the default `TextTheme`. Use this to specify the default
-        // text styling for headlines, titles, bodies of text, and more.
-        textTheme: const TextTheme(
-          headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
-          headline6: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
-          bodyText2: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
-        ),
-      ),
-      home: const MyHomePage(title: 'We-Mail'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+class ViewTemplate extends StatefulWidget {
+  const ViewTemplate({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<ViewTemplate> createState() => _ViewTemplateState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+class _ViewTemplateState extends State<ViewTemplate> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
